0c83966e165c1d9fde21b387ab8a95f552c1d364
vitor <vitor.grosskopf@gmail.com>
2023-05-01 14:12:21 -0300

    Restrict views from UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
 
@@ -22,4 +23,12 @@ class SessionsController < ApplicationController
     redirect_to login_path
   end
 
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
 end
